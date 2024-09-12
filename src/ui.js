d0c40b18f8ed52ca2fb8fd1666ff14813640e2ea
ryantz <ryantanzhi1997@gmail.com>
2024-09-12 10:51:24 +0800

    updated js method

--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,4 @@
+/*
 let bttButton = document.getElementById("bkToTop");
 
 window.onscroll = function () {scrollfunction()};
@@ -14,5 +15,21 @@ function topFunction() {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
 }
+*/
 
+let bttBtn = document.getElementById("bkToTop");
+
+window.onscroll = () => {scrollfunction()};
+
+function scrollfunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                bttBtn.style.display = "block"
+        } else {
+                bttBtn.style.display = "none"
+        }
+}
+
+bttBtn.addEventListener("click", () => {
+        window.scrollTo({top: 0, behavior: "smooth"});
+});
 
